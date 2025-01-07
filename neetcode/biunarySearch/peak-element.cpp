229bef9c07870a18528aa97b21107720338b6795
kunal <kunal.212034105@vcet.edu.in>
2025-01-07 23:34:44 +0530

    Add implementation for finding peak element in an array using binary search

---
+++ neetcode/biunarySearch/peak-element.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int findPeakElement(vector<int>& nums) {
+    int left = 0, right = nums.size() - 1;
+
+    while (left < right) {
+        int mid = left + (right - left) / 2;
+
+        if (nums[mid] > nums[mid + 1]) {
+            right = mid;
+        } else {
+            left = mid + 1;
+        }
+    }
+    return left;
+}
+
+int main() {
+    vector<int> nums = {1, 2, 3, 1};
+    int peakIndex = findPeakElement(nums);
+    cout << "Peak element index: " << peakIndex << endl;
+    return 0;
+}
